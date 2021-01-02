9c09fb22ceb3e5e87df9f3993bcbba9069e0e64d
Matt Decuir <decuirm@gmail.com>
2021-01-01 18:16:42 -0600

    Add /submissions page

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import { Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import Home from './home'
 import AllResolutions from './all-resolutions'
+import Submissions from './submissions'
 import '../styles/index.css'
 
 var history = createBrowserHistory()
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/all' component={AllResolutions} />
+        <Route path='/submissions' component={Submissions} />
       </Switch>
     </Router>
   )
---
+++ src/components/submissions.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import { loadAirtableData } from '../utils/airtable'
+
+const Submissions = () => {
+  const [resolutions, setResolutions] = useState([])
+  const [players, setPlayers] = useState([])
+
+  useEffect(() => {
+    const loadData = async () => {
+      // load resolutions
+      await loadAirtableData('2021', (results) => {
+        setResolutions((resolutions) => [...resolutions, ...results])
+      })
+      // load players
+      await loadAirtableData('Participants', (results) => {
+        setPlayers((players) => [...players, ...results])
+      })
+    }
+
+    loadData()
+  }, [])
+
+  let submitters = Object.fromEntries(players.map(person => [person.name, 0]))
+  resolutions.forEach((record) => {
+    submitters[record.submitter] = submitters[record.submitter] + 1 || 1
+  })
+
+  return (
+    <div>
+      <main>
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexFlow: 'column wrap'}}>
+          <h1 style={{textAlign: 'center', margin: '0', padding: '1em 1em 0.25em' }}>Resolutions Game</h1>
+          <div style={{textAlign: 'center', fontSize: '0.9em', fontStyle: 'italic', color: 'gray', paddingBottom: '1em'}}>
+            Strauss family new years, Jan 1, 2021
+          </div>
+          <h2 style={{textAlign: 'center', margin: '0', padding: '0.5em' }}>Submissions by person ({resolutions.length} total)</h2>
+          <ul style={{lineHeight: '2em'}}>
+            {Object.entries(submitters).map(([person, count], i) => {
+              return <li key={i}><span style={{fontWeight: 500, color: 'gray'}}>{person}</span>: {count}</li>
+            })}
+          </ul>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Submissions
